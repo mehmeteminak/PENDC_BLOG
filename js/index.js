87dec1d4537a7063958dbf028b6b7d13b9fc6688
Mehmet Emin Ak <mak44162@gmail.com>
2023-07-18 12:33:53 +0200

    Slider added

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
         slidesToScroll: 1,
         autoplay: !1,
         arrows:true,
-        prevArrow: $(".testimonial-carousel-controls .prev"),
-        nextArrow: $(".testimonial-carousel-controls .next"),
+        prevArrow: $(".carousel-container .prev"),
+        nextArrow: $(".carousel-container .next"),
         responsive: [{
             breakpoint: 1200,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 4
             }
         }, {
             breakpoint: 992,
@@ -25,3 +25,30 @@ $(document).ready(function() {
         }]
     });
 });
+
+function toggleMenu() {
+    
+    var sideMenu = document.getElementById("sideMenu");
+    sideMenu.classList.toggle("opened");
+}
+
+window.addEventListener('scroll', function() {
+
+    var targetDiv = this.document.getElementById('headerContact');
+    var header = this.document.getElementById('header');
+    var menuBar = this.document.getElementById('menuBar');
+    var logoBox = this.document.getElementById('logo');
+    
+    if (window.scrollY > 30) {
+        targetDiv.style.display = 'none';
+        
+        menuBar.style.height = "10vh";
+        logoBox.style.height = "10vh";
+        header.style.height = "10vh";
+    }else {
+        menuBar.style.height = "15vh";
+        logoBox.style.height = "15vh";
+        targetDiv.style.display = 'flex';
+        
+    }
+  });
